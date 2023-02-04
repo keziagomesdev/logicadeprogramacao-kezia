98b87c7a578e8a4b4153207983bac072e9312cc8
Kezia Gomes <dev.keziagomes@gmail.com>
2023-02-04 18:48:59 -0400

    primeiro codigo no github

---
+++ primeira-aula/primeiro_codigo.js
@@ -0,0 +1,7 @@
+var precos = [1200.8, 345.8, 2543.98];
+var total = // total dos valores produtos no array precos
+var incluirMedia = true;
+if(incluirMedia){
+    var total = total/precos.length;
+    //continuo a lógica
+}
